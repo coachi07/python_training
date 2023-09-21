04c46ab25b38459934ed49013fc70a4dd4d94f30
coachi07 <ayenistephen07@gmail.com>
2023-09-21 13:17:11 +0100

    i just added a function.py

---
+++ function.py
@@ -0,0 +1,37 @@
+# this will help uo create a new function
+def add():
+    a=6
+    b=5
+    print(a+b)
+
+add()
+
+def add2(a,b):
+    print("the total value is: " + str(a +b))
+
+add2(2,5)
+
+# task subract two numbers from each other
+
+def subract(a,b):
+    print("the total value is: " + str(a-b)) 
+    
+subract(15,11)
+
+def multiply (a,b):
+    print("the total value is: " + str(a*b)) 
+    
+multiply(15,11)
+
+def divide(a,b):
+    print("the total value is: " + str(a/b)) 
+    
+divide(75,15)
+
+def add2(a,b,c,d):
+    print("the total value is: " + str(a+b+c+d)) 
+    
+add2(15,11,10,9)
+
+
+
