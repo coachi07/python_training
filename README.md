8f3eac86563b13a8b1edfea4bf6eb0cd089a8539
coachi07 <ayenistephen07@gmail.com>
2023-09-21 11:25:29 +0100

    initial file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python_training
-This project is a text training for python classes
+This project is a text training for python classes, we are having our first python training here
---
+++ test1.py
@@ -0,0 +1,16 @@
+# write a python code that will add two numbers together
+a=6
+b=5
+print(a+b)
+
+# lets try to do a list and loopp throgh a list
+
+listoffruits=["orange","apple","pawpaw","cherry","mango"]
+
+fruitscount=[]
+
+for i in listoffruits:
+    print(i)
+    fruitscount.append(i)
+    print(fruitscount)
+
